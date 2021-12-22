35d167f19cfd321d0880dc00e2bd1c98b71a7161
dashlw <dash@dlwalton.com>
2021-12-21 17:39:23 -0700

    updated tests

--- src/pages/Search/Search.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Search from './index';
-
-describe('Search component', () => {
-  test('renders Search component',  () => {
-    render(<Search />);
-  });
-});
